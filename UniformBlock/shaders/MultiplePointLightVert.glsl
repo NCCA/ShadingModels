7cab5ad03391aca4606a44defabd6ee89d609d8a
jmacey <jmacey@bournemouth.ac.uk>
2015-11-19 10:30:06 +0000

    uniform block demo updated

--- UniformBlock/shaders/MultiplePointLightVert.glsl
+++ UniformBlock/shaders/MultiplePointLightVert.glsl
@@ -7,33 +7,6 @@ layout (location =2) in vec3 inNormal;
 /// @brief the in uv
 layout( location=1)in vec2 inUV;
 
-#define numLights 14
-
-
-layout (std140) uniform material
-{
-	// Ambient reflectivity
-	vec3 Ka;
-	// Diffuse reflectivity
-	vec3 Kd;
-	// Specular reflectivity
-	vec3 Ks;
-	// Specular shininess factor
-	float shininess;
-}mat;
-
-layout (std140) uniform  light
-{
-	// Light position in eye coords.
-	vec3 position;
-	// Ambient light intensity
-	vec3 La;
-	// Diffuse light intensity
-	vec3 Ld;
-	// Specular light intensity
-	vec3 Ls;
-}light[numLights];
-
 
 layout (std140) uniform transforms
 {
@@ -43,6 +16,34 @@ layout (std140) uniform transforms
 };
 
 
+
+#define numLights 20
+struct LightInfo
+{
+	// Light position in eye coords.
+	vec3 position;
+	// Ambient light intensity
+	vec3 La;
+	// Diffuse light intensity
+	vec3 Ld;
+	// Specular light intensity
+	vec3 Ls;
+};
+uniform LightInfo light[numLights];
+
+struct MaterialInfo
+{
+	// Ambient reflectivity
+	vec3 Ka;
+	// Diffuse reflectivity
+	vec3 Kd;
+	// Specular reflectivity
+	vec3 Ks;
+	// Specular shininess factor
+	float shininess;
+};
+
+uniform MaterialInfo material;
 
 // light output per vert
 out vec3 lightIntensity;
@@ -59,13 +60,13 @@ vec3 phongModel( int lightNum,vec4 position, vec3 norm )
 	vec3 s = normalize(vec3(light[lightNum].position - position.xyz));
 	vec3 v = normalize(-position.xyz);
 	vec3 r = reflect( -s, norm );
-	vec3 ambient = light[lightNum].La * mat.Ka;
+	vec3 ambient = light[lightNum].La * material.Ka;
 	float sDotN = max( dot(s,norm), 0.0 );
-	vec3 diffuse = light[lightNum].Ld * mat.Kd * sDotN;
+	vec3 diffuse = light[lightNum].Ld * material.Kd * sDotN;
 	vec3 spec = vec3(0.0);
 	if( sDotN > 0.0 )
 	{
-		spec = light[lightNum].Ls * mat.Ks * pow( max( dot(r,v), 0.0 ), mat.shininess );
+		spec = light[lightNum].Ls * material.Ks * pow( max( dot(r,v), 0.0 ), material.shininess );
 	}
 	return ambient + diffuse + spec;
 }
@@ -78,14 +79,12 @@ void main()
 	getEyeSpace(eyeNorm, eyePosition);
 	// Evaluate the lighting equation.
 	lightIntensity=vec3(0.0);
-	for(int i=0; i<numLights; ++i)
+	for(int i=0; i<20; ++i)
 	{
 		lightIntensity += phongModel( i,eyePosition, eyeNorm );
 	}
-	//lightIntensity=light[0].La;
 	// Convert position to clip coordinates and pass along
 	gl_Position = MVP*vec4(inVert,1.0);
-
 }
 
 
